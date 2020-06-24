12df14c3983eaad4d93dd17e49bb4af68455e234
bobrayEden <o.nocaudie@googlemail.com>
2020-06-24 18:13:56 +0200

    init

---
+++ src/main/java/com/bobray/beernextdoor/BeernextdoorApplication.java
@@ -0,0 +1,13 @@
+package com.bobray.beernextdoor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeernextdoorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeernextdoorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bobray/beernextdoor/BeernextdoorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bobray.beernextdoor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeernextdoorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
