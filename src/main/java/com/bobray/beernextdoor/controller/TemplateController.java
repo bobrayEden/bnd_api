bbc04030c9865167713ebdbe7a8ed4a1f4337354
bobrayEden <o.nocaudie@googlemail.com>
2020-07-24 17:51:33 +0200

    pas mal de html la navigation sauf log et sign spring secu viré pour cause de pénibilité, à voir plus tard

--- src/main/java/com/bobray/beernextdoor/controller/TemplateController.java
+++ src/main/java/com/bobray/beernextdoor/controller/TemplateController.java
@@ -10,4 +10,34 @@ public class TemplateController {
     public String getIndex() {
         return "index";
     }
+
+    @GetMapping("/user-profile")
+    public String getProfile() {
+        return "user-profile";
+    }
+
+    @GetMapping("/type-form")
+    public String getTypeForm() {
+        return "type-form";
+    }
+
+    @GetMapping("/brewery-form")
+    public String getBreweryForm() {
+        return "brewery-form";
+    }
+
+    @GetMapping("/beer-form")
+    public String getBeerForm() {
+        return "beer-form";
+    }
+
+    @GetMapping("/store-form")
+    public String getStoreForm() {
+        return "store-form";
+    }
+
+    @GetMapping("/out")
+    public String getOut() {
+        return "redirect:/";
+    }
 }
