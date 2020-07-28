f4d9373f92db1afcf3a85d0805040d63d26d7465
bobrayEden <o.nocaudie@googlemail.com>
2020-07-28 17:56:35 +0200

    affichage des listes et select

--- src/main/java/com/bobray/beernextdoor/controller/TemplateController.java
+++ src/main/java/com/bobray/beernextdoor/controller/TemplateController.java
@@ -1,14 +1,33 @@
 package com.bobray.beernextdoor.controller;
 
-import com.bobray.beernextdoor.entity.User;
+import com.bobray.beernextdoor.entity.*;
+import com.bobray.beernextdoor.repository.BeerRepository;
+import com.bobray.beernextdoor.repository.BreweryRepository;
+import com.bobray.beernextdoor.repository.StoreRepository;
+import com.bobray.beernextdoor.repository.TypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class TemplateController {
+
+    @Autowired
+    TypeRepository typeRepository;
+
+    @Autowired
+    BreweryRepository breweryRepository;
+
+    @Autowired
+    StoreRepository storeRepository;
+
+    @Autowired
+    BeerRepository beerRepository;
 
     @GetMapping("/")
     public String getIndex() {
@@ -43,22 +62,30 @@ public class TemplateController {
     }
 
     @GetMapping("/type-form")
-    public String getTypeForm() {
+    public String getTypeForm(Model out) {
+        List<Type> types = typeRepository.findAll();
+        out.addAttribute("types", types);
         return "type-form";
     }
 
     @GetMapping("/brewery-form")
-    public String getBreweryForm() {
+    public String getBreweryForm(Model out) {
+        List<Brewery> breweries = breweryRepository.findAll();
+        out.addAttribute("breweries", breweries);
         return "brewery-form";
     }
 
     @GetMapping("/beer-form")
-    public String getBeerForm() {
+    public String getBeerForm(Model out) {
+        List<Beer> beers = beerRepository.findAll();
+        out.addAttribute("beers", beers);
         return "beer-form";
     }
 
     @GetMapping("/store-form")
-    public String getStoreForm() {
+    public String getStoreForm(Model out) {
+        List<Store> stores = storeRepository.findAll();
+        out.addAttribute("stores", stores);
         return "store-form";
     }
 
